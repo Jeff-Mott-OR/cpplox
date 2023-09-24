ab1a6fb70cd1f95fd09c9a3516cccab3dd2a6d6a
Jeff Mott <jeff.mott.or@gmail.com>
2023-09-23 17:18:05 -0700

    Remove old implementation

--- project/bench/scripts/fib.js
+++
@@ -1,8 +0,0 @@
-function fib(n) {
-  if (n < 2) return n;
-  return fib(n - 2) + fib(n - 1);
-}
-
-let start = new Date().getTime();
-console.log(fib(15) === 610);
-console.log(new Date().getTime() - start);
--- project/src/treewalk_interpreter/statement_visitor_fwd.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace motts { namespace lox {
-    struct Stmt_visitor;
-}}
